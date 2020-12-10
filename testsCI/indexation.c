f93af4b1262555b0e0561a416bc9fb59ee82d7c8
Mathias Bsrt <mathias.bossaerts9581@gmail.com>
2020-12-10 15:57:52 +0100

    test indexation conception + test echec

--- testsCI/indexation.c
+++ testsCI/indexation.c
@@ -4,7 +4,19 @@
 //#SI notre prog de test retourn 1 : echec de la pipeline
 
 int indexationImage(){
-    return 0;
+    //Lancer une indexation pour tous les fichiers du dossier de test
+    //On récupere les fichiers générés base_descripteur_image et lier_base_image
+    //On compare ces deux fichiers avec nos fichiers base_descripteur_image_OK et lier_base_image_OK => ces deux fichiers sont une version confirmée de nos indexations
+    int res = 1;
+    
+    //if base_descripteur_image != base_descripteur_OK || lier_base_image != lier_base_image_OK
+
+    if(res){
+        printf("ERREUR DANS L'INDEXATION DES IMAGES");
+    }else{
+        printf("SUCCES DANS L'INDEXATION DES IMAGES");
+    }
+    return res;
 }
 
 int indexationTexte(){
