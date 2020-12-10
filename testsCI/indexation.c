424f4b5be16180c81e5a76760526b15d23099bf5
Mathias Bsrt <mathias.bossaerts9581@gmail.com>
2020-12-10 16:02:41 +0100

    essai pour failed un job

--- testsCI/indexation.c
+++ testsCI/indexation.c
@@ -2,13 +2,14 @@
  * Ici les tests de l'indexation
  */
 //#SI notre prog de test retourn 1 : echec de la pipeline
+#include <stdio.h>
 
 int indexationImage(){
     //Lancer une indexation pour tous les fichiers du dossier de test
     //On récupere les fichiers générés base_descripteur_image et lier_base_image
     //On compare ces deux fichiers avec nos fichiers base_descripteur_image_OK et lier_base_image_OK => ces deux fichiers sont une version confirmée de nos indexations
     int res = 1;
-    
+
     //if base_descripteur_image != base_descripteur_OK || lier_base_image != lier_base_image_OK
 
     if(res){
@@ -29,10 +30,10 @@ int indexationSon(){
 
 int main(int argc, char const *argv[])
 {
-    int res = 0;;
-    res = indexationImage();
-    res = indexationSon();
-    res = indexationTexte();
+    int res = 0;
+    res = res || indexationImage();
+    res = res || indexationSon();
+    res = res || indexationTexte();
     
     return res;
 }
