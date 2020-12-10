29375756aeb13e28e367d66937725ed2f495f82e
Mathias Bsrt <mathias.bossaerts9581@gmail.com>
2020-12-10 16:06:32 +0100

    test indextion conception finale

--- testsCI/indexation.c
+++ testsCI/indexation.c
@@ -8,7 +8,7 @@ int indexationImage(){
     //Lancer une indexation pour tous les fichiers du dossier de test
     //On récupere les fichiers générés base_descripteur_image et lier_base_image
     //On compare ces deux fichiers avec nos fichiers base_descripteur_image_OK et lier_base_image_OK => ces deux fichiers sont une version confirmée de nos indexations
-    int res = 1;
+    int res = 0;
 
     //if base_descripteur_image != base_descripteur_OK || lier_base_image != lier_base_image_OK
 
