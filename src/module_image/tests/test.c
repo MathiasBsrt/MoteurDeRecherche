274880dc8ffc7e04fc2c5cba2665a65f57c7728d
Mathias Bsrt <mathias.bossaerts9581@gmail.com>
2020-12-14 13:38:43 +0100

    add test indexation image

---
+++ src/module_image/tests/test.c
@@ -0,0 +1,15 @@
+
+int test_RGB(){
+    //lancer une indexation du dossier TEST_RGB
+    
+    //Recuperer base_descripteur_image_OK
+
+    // Comparer base_descripteur_image et base_descripteur_image_OK
+    //Utilistion de diff (cmd unix)
+
+}
+int main(int argc, char const *argv[])
+{
+    test_RGB();
+    return 0;
+}
