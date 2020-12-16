02a6801d3bb3c746f3829b275f9b3068acecb99c
Mathias Bsrt <mathias.bossaerts9581@gmail.com>
2020-12-16 14:03:12 +0100

    changements test indexation images

--- src/module_image/tests/test.c
+++ src/module_image/tests/test.c
@@ -1,12 +1,21 @@
+#include "controle_descripteur.h"
+#include <string.h>
+#include <stdio.h>
 
 int test_RGB(){
     //lancer une indexation du dossier TEST_RGB
+    char cheminFichier[] = "";
     
-    //Recuperer base_descripteur_image_OK
+     // pour chaque fichier d'un dossier
 
     // Comparer base_descripteur_image et base_descripteur_image_OK
     //Utilistion de diff (cmd unix)
 
+    char cmd[100] = "diff ../base_descripteur_image base_descripteur_image_OK";
+
+    int resCmd = system(cmd); //retourne 0 si égaux
+
+    return resCmd;
 }
 int main(int argc, char const *argv[])
 {
