a264f3e107833bd93588696812ef67c39ff14364
Mathias Bsrt <mathias.bossaerts9581@gmail.com>
2020-12-17 20:10:34 +0100

    ajout test indexation image

---
+++ testsCI/indexationImage.c
@@ -0,0 +1,30 @@
+/***
+ * Ici les tests de l'indexation
+ */
+//#SI notre prog de test retourn 1 : echec de la pipeline
+#include <stdio.h>
+// Includes pour les tests image
+#include "../src/module_image/controle_descripteur.h"
+
+
+int indexationImage(){
+    creationDescripteur("../src/module_image/tests/TEST_RGB/txt/01.txt"); // Génération rgb
+    creationDescripteur("../src/module_image/tests/TEST_NB/txt/51.txt"); // Génératio nb
+    
+    //Utilistion de diff (cmd unix)
+    char cmd[100] = "diff base_descripteur_image ./TEST_IMAGE/base_descripteur_image_OK";
+
+    int resCmd = system(cmd); //retourne 0 si égaux
+
+    char cmd2[100] = "diff liste_base_image ./TEST_IMAGE/liste_base_image_OK";
+    resCmd = resCmd || system(cmd2); //retourne 0 si égaux
+
+    return resCmd;
+}
+
+int main(int argc, char const *argv[])
+{
+
+    return indexationImage();
+
+}
--- testsCI/indexationSon.c
+++ testsCI/indexationSon.c
@@ -9,25 +9,6 @@
 #include "../src/module_audio/histogramme.h"
 #include "../src/module_audio/base_descripteur.h"
 
-int indexationImage(){
-    //Lancer une indexation pour tous les fichiers du dossier de test
-    //On récupere les fichiers générés base_descripteur_image et lier_base_image
-    //On compare ces deux fichiers avec nos fichiers base_descripteur_image_OK et lier_base_image_OK => ces deux fichiers sont une version confirmée de nos indexations
-    int res = 0;
-
-    //if base_descripteur_image != base_descripteur_OK || lier_base_image != lier_base_image_OK
-
-    if(res){
-        printf("ERREUR DANS L'INDEXATION DES IMAGES");
-    }else{
-        printf("SUCCES DANS L'INDEXATION DES IMAGES");
-    }
-    return res;
-}
-
-int indexationTexte(){
-    return 0;
-}
 
 int indexationSon(){
 
@@ -162,10 +143,7 @@ int indexationSon(){
 
 int main(int argc, char const *argv[])
 {
-    int res = 0;
-    res = res || indexationImage();
-    res = res || indexationSon();
-    res = res || indexationTexte();
     
-    return res;
+    return indexationSon();
+
 }
